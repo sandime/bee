c9ce7edf5d5e89660c48f72578d5c65f277aa670
sherri <sherri@sherricruz.com>
2015-03-31 14:55:59 -0700

    fetches flags

--- main.js
+++ main.js
@@ -1,36 +1,22 @@
 /**
  * Created by SHERRI on 3/27/15.
  */
-/* this goes away
-var countryApp = angular.module('countryApp', []);
-countryApp.controller('CountryListCtrl', function ($scope, $http){
-    $http.get('countries.json').success(function(data) {
-        $scope.countries = data;
-    });
-});
-
-*/
-
-//new
-
-var countryApp = angular.module('countryApp', ['ngRoute']);
-
-countryApp.config(function($routeProvider) {
-    $routeProvider.
-        when('/', {
+   var countryApp = angular.module('countryApp', ['ngRoute']);
+      countryApp.config(function($routeProvider) {
+        $routeProvider.
+          when('/', {
             templateUrl: 'country-list.html',
             controller: 'CountryListCtrl'
-        }).
-        when('/:countryId', {
+          }).
+          when('/:countryId', {
             templateUrl: 'country-detail.html',
             controller: 'CountryDetailCtrl'
-        }).
-        otherwise({
+          }).
+          otherwise({
             redirectTo: '/'
-        });
-});
-/* querying a database based on id #45 as below*/
-countryApp.factory('countries', function($http){
+          });
+      });
+      countryApp.factory('countries', function($http){
         return {
           list: function (callback){
             $http({
@@ -48,66 +34,27 @@ countryApp.factory('countries', function($http){
           }
         };
       });
-
-// directive
-
-countryApp.directive('country', function(){
-    return {
-        scope: {
-            country: '=' //bidirectional binding; directives have own scope
-        },
-        restrict: 'A',
-        templateUrl: 'country.html'
-    };
-});
-//gets countries factory's info and assign it to $scope.countries
+      countryApp.directive('country', function(){
+        return {
+          scope: {
+            country: '='
+          },
+          restrict: 'A',
+          templateUrl: 'country.html',
+          controller: function($scope, countries){
+            countries.find($scope.country.id, function(country) {
+              $scope.flagURL = country.flagURL;
+            });
+          }
+        };
+      });
       countryApp.controller('CountryListCtrl', function ($scope, countries){
         countries.list(function(countries) {
           $scope.countries = countries;
         });
       });
-
       countryApp.controller('CountryDetailCtrl', function ($scope, $routeParams, countries){
         countries.find($routeParams.countryId, function(country) {
           $scope.country = country;
         });
       });
-
-
-
-/*
-countryApp.factory('countries', function($http){
-
-        function getData(callback){
-          $http({
-            method: 'GET',
-            url: 'countries.json',
-            cache: true
-          }).success(callback);
-        }
-
-        return {
-          list: getData,
-          find: function(name, callback){
-            getData(function(data) {
-              var country = data.filter(function(entry){
-                return entry.name === name;
-              })[0];
-              callback(country);
-            });
-          }
-        };
-      });
-
-      countryApp.controller('CountryListCtrl', function ($scope, countries){
-        countries.list(function(countries) {
-          $scope.countries = countries;
-        });
-      });
-
-      countryApp.controller('CountryDetailCtrl', function ($scope, $routeParams, countries){
-        countries.find($routeParams.countryName, function(country) {
-          $scope.country = country;
-        });
-      });
-*/
